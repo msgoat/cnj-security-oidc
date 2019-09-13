01b31d3e49d028df30226676d67c6edb05aa7eee
Michael Theis <michael.theis@msg.group>
2019-09-13 17:02:35 +0200

    patched version 2.0.0
    - added @Consumes and @Produces to all JAX-RS resource classes.

--- cnj-security-oidc-backend-micro/src/main/java/group/msg/at/cloud/cloudtrain/adapter/rest/HelloResource.java
+++ cnj-security-oidc-backend-micro/src/main/java/group/msg/at/cloud/cloudtrain/adapter/rest/HelloResource.java
@@ -5,8 +5,11 @@ import group.msg.at.cloud.cloudtrain.core.boundary.HelloWorld;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -22,6 +25,8 @@ import javax.ws.rs.core.Response;
  */
 @RequestScoped
 @Path("v1/hello")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class HelloResource {
 
     @Inject
