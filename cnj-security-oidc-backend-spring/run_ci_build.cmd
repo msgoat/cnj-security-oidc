fb58c3217620b9d81cee862cd4f62f213ab6024e
Michael Theis <michael.theis@msg.group>
2019-09-16 18:14:55 +0200

    released version 2.0.1
    - reconfigured after switch to newly setup Kubernetes cluster

--- cnj-security-oidc-backend-spring/run_ci_build.cmd
+++ cnj-security-oidc-backend-spring/run_ci_build.cmd
@@ -1,8 +1,12 @@
-call mvn clean install -P commit-stage
+setlocal
+
+:: call mvn clean install -P commit-stage
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-call mvn clean install -P dockerize-stage -Dtest.target.route=http://192.168.99.100:8080
+:: call mvn clean install -P dockerize-stage -Dtest.target.route=http://localhost:38080
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-call mvn clean verify -P integration-test-stage
+call mvn clean verify -P integration-test-stage -Dtest.target.route=http://k8sapps.at.automotive.msg.team/cnj-security-oidc-backend-spring
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+endlocal
